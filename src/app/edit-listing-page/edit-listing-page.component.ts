6821939206b6c9478dcc9ae8bf963c69dd6a5668
honorezemagho <honorezemagho@gmail.com>
2020-09-17 07:05:34 +0100

    finish with edit listing component

--- src/app/edit-listing-page/edit-listing-page.component.ts
+++ src/app/edit-listing-page/edit-listing-page.component.ts
@@ -1,4 +1,7 @@
+import { fakeMyListings } from './../fake-data';
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { Listing } from '../types';
 
 @Component({
   selector: 'app-edit-listing-page',
@@ -6,10 +9,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./edit-listing-page.component.css']
 })
 export class EditListingPageComponent implements OnInit {
+  listing: Listing;
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute
+  ) { }
 
   ngOnInit() {
+    const id = this.route.snapshot.paramMap.get('id');
+    this.listing = fakeMyListings.find(listing => listing.id == id);
+  }
+
+  onSubmit(){
+    alert("Saving changes to the listing...");
+    this.router.navigateByUrl('my-listings');
   }
 
 }
