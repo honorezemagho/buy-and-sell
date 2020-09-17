3add1b8d03a6673d16da9ef91b67125918a9b2b2
honorezemagho <honorezemagho@gmail.com>
2020-09-17 05:35:12 +0100

    contact page finish

--- src/app/contact-page/contact-page.component.ts
+++ src/app/contact-page/contact-page.component.ts
@@ -1,4 +1,7 @@
+import { fakeListings } from './../fake-data';
 import { Component, OnInit } from '@angular/core';
+import { Listing } from '../types';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-contact-page',
@@ -6,10 +9,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact-page.component.css']
 })
 export class ContactPageComponent implements OnInit {
+  email: string = '';
+  message: string = '';
+  listing: Listing;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+  ) { }
 
   ngOnInit() {
+    const id = this.route.snapshot.paramMap.get('id');
+    this.listing = fakeListings.find(listing => listing.id === id);
+    this.message = `Hi, I'm interesed in your ${this.listing.name.toLowerCase()}`
+  }
+
+  sendMessage(): void {
+    alert('Your message has been sent!');
+    this.router.navigateByUrl('/listings');
   }
 
 }
