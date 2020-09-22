fcc810f739a436183ff9a1e938b0f91199ebc1d3
honorezemagho <honorezemagho@gmail.com>
2020-09-22 16:08:30 +0100

    add listings service to share  listings to component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
+import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms'
+import {HttpClientModule} from '@angular/common/http';
+
+
 import { AppComponent } from './app.component';
 import { ListingsPageComponent } from './listings-page/listings-page.component';
 import { ListingDetailPageComponent } from './listing-detail-page/listing-detail-page.component';
@@ -9,9 +12,9 @@ import { ContactPageComponent } from './contact-page/contact-page.component';
 import { MyListingsPageComponent } from './my-listings-page/my-listings-page.component';
 import { NewListingPageComponent } from './new-listing-page/new-listing-page.component';
 import { EditListingPageComponent } from './edit-listing-page/edit-listing-page.component';
-import {FormsModule} from '@angular/forms';
 import { ListingDataFormComponent } from './listing-data-form/listing-data-form.component';
 import { NavbarComponent } from './navbar/navbar.component';
+
 
 @NgModule({
   declarations: [
@@ -23,12 +26,13 @@ import { NavbarComponent } from './navbar/navbar.component';
     NewListingPageComponent,
     EditListingPageComponent,
     ListingDataFormComponent,
-    NavbarComponent
+    NavbarComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/listings.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {Listing} from './types';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+
+export class ListingsService {
+
+  constructor(
+  	private http: HttpClient
+  ) { }
+
+  getListings() : Observable<Listing[]> {
+  	return this.http.get<Listing[]>('http://localhost:8000/api/listings');
+  }
+}
